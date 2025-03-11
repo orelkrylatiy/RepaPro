95491aa9c054528bf8681e3a31e7ca8d08acf26f
orelkrylatiy <maxim700a@gmail.com>
2025-03-11 19:03:46 +0500

    finished creating site

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-'use strict';
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("contact-form");
 
-console.log(document.querySelectorAll('.circle'));
+    form.addEventListener("submit", async function (event) {
+        event.preventDefault(); // Останавливаем стандартную отправку формы
+
+        const formData = new FormData(form);
+        const data = {
+            name: formData.get("name"),
+            contact: formData.get("contact"),
+            details: formData.get("details"),
+        };
+        try {
+            const response = await fetch("https://script.google.com/macros/s/AKfycbzo6nuuSASOP_Z0008Mrorlr7lI8KbxpMr6ZX5_j94dJkXsP-8Jr664onunqQRmHw-u/exec", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+                mode: 'no-cors', // Disable CORS
+            });
+            alert("Данные отправлены: " + result.status);
+        } catch (error) {
+            form.reset();
+            alert("Данные отправлены: " + result.status);
+        }
+        form.reset();
+    });
+});
